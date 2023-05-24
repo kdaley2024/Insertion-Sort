be43ff344d518e9ef7f29cc6c729ceb920e0d2e9
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2023-05-23 23:22:31 -0400

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,1 +1,30 @@
-
+// Java program for implementation of Insertion Sort
+public class InsertionSort {
+    /*Function to sort array using insertion sort*/
+    void sort(int arr[])
+    {
+      // TODO
+    }
+ 
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+ 
+        System.out.println();
+    }
+ 
+    // Driver method
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6 };
+ 
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+ 
+        printArray(arr);
+    }
+};
+ 
