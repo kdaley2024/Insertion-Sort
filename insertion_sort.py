b132f44bafb7146331749ac532cbabf8827aaa58
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2023-05-24 00:07:09 -0400

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+# Python program for implementation of Insertion Sort
+ 
+# Function to do insertion sort
+def insertionSort(arr):
+  # TODO 
+ 
+ 
+# Driver code to test above
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+for i in range(len(arr)):
+    print ("% d" % arr[i])
