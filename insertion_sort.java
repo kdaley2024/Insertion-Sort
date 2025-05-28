2fa2bc29a998ec5ff2087ef6b40c7db42c6f0725
kdaley2024 <kdaley2024@my.fit.edu>
2025-05-28 17:50:33 -0400

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,9 +1,21 @@
 // Java program for implementation of Insertion Sort
 public class InsertionSort {
     /*Function to sort array using insertion sort*/
-    void sort(int arr[])
-    {
-      // TODO
+    void sort(int arr[]) {
+        // TODO
+        int key,i;
+        for (int j = 2; j < arr.length; i++) {
+            key = arr[j];
+            //Insert arr[j] into the sorted sequence arr[1..j-1]
+            i = j-1;
+            while (i > 0 && arr[i] > key) {
+                arr[i + 1] = arr[i];
+                i = i - 1;
+            }
+            arr[i + 1] = key
+        }
+            
+        
     }
  
     /* A utility function to print array of size n*/
