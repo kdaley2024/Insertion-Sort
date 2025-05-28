7c58d418ded978c94727a9666617c15c99f3244d
kdaley2024 <kdaley2024@my.fit.edu>
2025-05-28 19:29:01 -0400

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,20 +1,64 @@
 // Java program for implementation of Insertion Sort
 public class InsertionSort {
     /*Function to sort array using insertion sort*/
-    void sort(int arr[])
-    {
-      // TODO
+    void sort(int arr[]) {
+        // TO DO
+        int key,i,x;
+        x = 1;
+        System.out.print(x + ": ");
+        printArray(arr,1);
+        x++;
+        for (int j = 1; j < arr.length; j++) {
+            key = arr[j];
+            //Insert arr[j] into the sorted sequence arr[1..j-1]
+            i = j-1;
+            while (i >= 0 && arr[i] > key) {
+                arr[i + 1] = arr[i];
+                i = i - 1;
+            }
+            arr[i + 1] = key;
+            System.out.print(x + ": ");
+            printArray(arr, j+1);
+            x++;
+
+        }
+            
+        
     }
  
     /* A utility function to print array of size n*/
     static void printArray(int arr[])
     {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
- 
+        System.out.print("\nFinal result: [");
+        for (int i = 0; i < n-1; ++i) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.print(arr[n-1]);
+        System.out.print("]");
         System.out.println();
     }
+
+    static void printArray(int arr[], int key)
+    {
+            int n = arr.length;
+            System.out.print("[");
+            for (int i = 0; i < n-1; ++i) {
+                if (key == i) {
+                    System.out.print("*");
+                    System.out.print(arr[i]);
+                    System.out.print("*");
+                    System.out.print(", ");
+                } else {
+                    System.out.print(arr[i]);
+                    System.out.print(", ");
+                }
+            }
+    
+            System.out.print(arr[n-1]);
+            System.out.print("]");
+            System.out.println();
+        }
  
     // Driver method
     public static void main(String args[])
@@ -23,8 +67,10 @@ public class InsertionSort {
  
         InsertionSort ob = new InsertionSort();
         ob.sort(arr);
- 
+
+        
         printArray(arr);
+
     }
 };
  
